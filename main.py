9e2c3152233d3034cff794a9944ed24da2b928d8
Olena Surilova <elen21surilova@gmail.com>
2026-02-14 18:35:21 +0100

    feat: draw a Dashed Line with Turtle GUI

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("MediumSlateBlue")
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+# === Square ===
+# for _ in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+
+# === Dashed Line ===
+for i in range(20):
+    timmy_the_turtle.pendown()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.setx(i * 20)
 
 
 screen = Screen()
