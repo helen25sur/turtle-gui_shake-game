542284b9250a3126acc18d2de43d1bba09b0c743
Olena Surilova <elen21surilova@gmail.com>
2026-02-14 22:11:01 +0100

    fix: change start position for drawing dots

--- hirst-painting.py
+++ hirst-painting.py
@@ -21,20 +21,26 @@ screen.colormode(255)
 
 color_list = [(228, 227, 226), (246, 237, 243), (243, 244, 246), (234, 166, 59), (45, 112, 157), (113, 150, 203), (212, 123, 164), (16, 128, 96), (172, 44, 88), (1, 177, 143), (153, 18, 54), (224, 201, 117), (225, 76, 115), (163, 166, 35), (28, 35, 84), (227, 86, 43), (42, 166, 208), (120, 172, 116), (119, 102, 158), (215, 64, 33), (237, 244, 241), (39, 52, 100), (76, 21, 45), (229, 169, 188), (14, 99, 71), (31, 61, 56), (8, 92, 107), (222, 177, 169), (181, 188, 208), (171, 203, 193)]
 
-# timmy_the_turtle.setheading(225)
-# timmy_the_turtle.forward(250)
-# timmy_the_turtle.setheading(0)
+timmy_the_turtle.setheading(225)
+timmy_the_turtle.penup()
+timmy_the_turtle.forward(300)
+timmy_the_turtle.setheading(0)
 
-for i in range(1, 11):
-    for _ in range(10):
-        timmy_the_turtle.pendown()
-        timmy_the_turtle.dot(20, random.choice(color_list))
-        timmy_the_turtle.penup()
-        timmy_the_turtle.forward(40)
+number_of_dots = 100
 
-    timmy_the_turtle.setposition(0, i * 40)
-    timmy_the_turtle.left(90)
-    timmy_the_turtle.right(90)
+for dot_count in range(1, number_of_dots + 1):
+    timmy_the_turtle.pendown()
+    timmy_the_turtle.dot(20, random.choice(color_list))
+    timmy_the_turtle.penup()
+    timmy_the_turtle.hideturtle()
+    timmy_the_turtle.forward(50)
+
+    if dot_count % 10 == 0:
+        timmy_the_turtle.setheading(90)
+        timmy_the_turtle.forward(50)
+        timmy_the_turtle.setheading(180)
+        timmy_the_turtle.forward(500)
+        timmy_the_turtle.setheading(0)
 
 screen.tracer(0)
 
