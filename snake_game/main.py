d707637ce1eeaa907d11719a51e0d2dd85ef76a2
Olena Surilova <elen21surilova@gmail.com>
2026-02-15 14:48:59 +0100

    feat: create a snake body

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,8 +1,26 @@
 # TODO
 # 1. Create a snake body
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+list_segment = []
+
+for i in range(3):
+    new_turtle = Turtle()
+    new_turtle.shape("square")
+    new_turtle.color("white")
+    new_turtle.goto(-i*20, 0)
+    list_segment.append(new_turtle)
+
 # 2. Move the snake
 # 3. Control the snake
 # 4. Detect collision with food
 # 5. Create a scoreboard
 # 6. Detect collision with wall
 # 7. Detect collision with tail
+
+screen.exitonclick()
